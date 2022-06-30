2fa559fe298d1fb4d12e14731fc847e892ef8c20
Adil Zuhri <106576506+adilzuhri@users.noreply.github.com>
2022-07-01 00:48:05 +0800

    Added Day1

---
+++ MyApp/Core/MyFirstApp.java
@@ -0,0 +1,34 @@
+ // Package declaration
+ package myapp.core
+
+ // Imports - what to use from other packages
+
+ // Java program is class
+ public class MyFirstApp {
+
+ }
+
+        // Program entry point, method with a specific signature
+        Run|Debug
+        public static void mainname(String [] args) {
+          LOKO  System.out.println("hello, world");
+            
+            
+        }
+// Imports - what to use from other packages
+
+ // Java program is class
+ public class MyFirstApp {
+
+}SAC
+
+       // Program entry point, method with a specific signature
+       Run|Debug
+       public static void mainname(String [] args) {
+         LOKO  System.out.println("hello, world");
+           
+// Convert string to integer
+
+
+
+        System out someghing tsomething 
